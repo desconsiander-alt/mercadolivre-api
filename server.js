8881264b9f68dae4fdebefe573a0a28c6fde0fdf
desconsiander-alt <desconsiander@gmail.com>
2026-08-03 15:26:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+
+const produtoRoute = require("./routes/produto");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({
-        status: "online",
-        mensagem: "API Mercado Livre funcionando!"
-    });
+  res.json({
+    status: "online",
+    mensagem: "API Mercado Livre funcionando!"
+  });
 });
 
+app.use("/produto", produtoRoute);
+
+const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado na porta ${PORT}`);
+  console.log(`Servidor iniciado na porta ${PORT}`);
 });
